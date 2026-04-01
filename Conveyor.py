d5dc595cd1e5df340db9ab32329c056cfed19d3f
Moody-Jawhar <jawharmoe@hotmail.com>
2026-03-31 22:41:22 -0400

    Update Conveyor.py

--- Conveyor.py
+++ Conveyor.py
@@ -2,6 +2,14 @@ from utils.brick import Motor, EV3GyroSensor, EV3ColorSensor, configure_ports, b
 import math
 import threading
 
+COLOR_SENSOR, GYRO, LEFT_MOTOR, RIGHT_MOTOR, CONVEYOR, ROTATING_MOTOR = configure_ports(
+    PORT_1=EV3ColorSensor,
+    PORT_3=EV3GyroSensor,
+    PORT_A=Motor,
+    PORT_B=Motor,
+    PORT_C=Motor,
+    PORT_D=Motor,
+)
 # ==========================================================================
 # CONVEYOR HELPERS
 # ==========================================================================
