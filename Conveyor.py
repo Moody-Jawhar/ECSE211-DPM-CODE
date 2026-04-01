a6259bf38d16bdf80da7717ea3a4f0e4d4b40789
Moody-Jawhar <jawharmoe@hotmail.com>
2026-03-31 22:38:55 -0400

    Update Conveyor.py

--- Conveyor.py
+++ Conveyor.py
@@ -1,1 +1,11 @@
 
+
+
+# ==========================================================================
+# CONVEYOR HELPERS
+# ==========================================================================
+def pickup_brick():
+    CONVEYOR.set_position_relative(220)
+
+def throw_brick():
+    CONVEYOR.set_position_relative(-250)
